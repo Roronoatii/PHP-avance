bcf50f89feaff97a8cacbe571fc2bf1101730f25
justoverfr <justover.fr@gmail.com>
2023-01-17 15:25:23 +0100

    create account and user class

--- src/db.php
+++ src/db.php
@@ -3,11 +3,24 @@
 require_once __DIR__ . '/config.php';
 
 try {
-	$dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'].';port='.$config['db']['port'].'';
+	$dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'] . ';port=' . $config['db']['port'] . '';
 	$db = new PDO($dsn, $config['db']['user'], $config['db']['pass']);
 
 } catch (Exception $e) {
 	die('Erreur MySQL. ' . $e->getMessage());
 }
 
+function createAccount($firstname, $lastname, $email, $password, $birthdate)
+{
+	global $dbManager;
+
+	$iban = "FR";
+	for ($i = 0; $i < 5; $i++) {
+		$iban .= strval(rand(10000, 99999));
+	}
+
+	$sql = "INSERT INTO `users` (`firstname`, `lastname`, `mail`, `password`, `birthdate`, `iban`)
+	VALUES (?, ?, ?, ?, ?, ?)";
+	DbManager->insert($sql, [$firstname, $lastname, $email, $password, $birthdate, $iban]);
+}
 ?>
