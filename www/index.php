2b6aa1ab56b313fcb6a54a15c8a825cc9d6d7c74
Paul <p.rivallin@protonmail.com>
2023-01-17 14:40:25 +0100

    index

--- www/index.php
+++ www/index.php
@@ -12,20 +12,11 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 <body>
 
     <div>
-        <h1>Home page</h1>
+        <h1>LA SUPER BANQUE</h1>
+
+        <h8>La banque qui vous accompagne</h8>
     </div>
 
-    <?php
-    $sql = "INSERT INTO contact_forms (fullname, phone, email, message) VALUES (?, ?, ?, ?)";
-    $data = [
-        'John Doe',
-        '0123456789',
-        'fsef',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-    ];
-
-    $db->insert($sql, $data);
-    ?>
 
     <?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
 </body>
