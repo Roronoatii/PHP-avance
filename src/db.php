7034bc1d6196ed7802f16d7b4b07db2a3e8e49c2
Paul <p.rivallin@protonmail.com>
2023-01-18 07:54:06 +0100

    mini correc

--- src/db.php
+++ src/db.php
@@ -23,10 +23,10 @@ function createAccount($firstname, $lastname, $email, $password, $birthdate)
 	VALUES (?, ?, ?, ?, ?, ?)";
 	$dbManager->insert($sql, [$firstname, $lastname, $email, $password, $birthdate, $iban]);
 }
-function requestRequired($role, $requiredLvl){
+function requestRequired($roles, $requiredLvl){
 	global $dbManager;
 
-		$role = $dbManager->getBy('roles', 'name', $role, 'User');
+		$role = $dbManager->getBy('roles', 'name', $roles, 'User'); //name is not id
 
 	if($role['id'] < $requiredLvl) {
 		// header('contact.php');
