a64d2998baf9d149bdd3a01298ffaa3a3700a644
justoverfr <jeanjuste.lefebvre@gmail.com>
2023-01-18 18:02:51 +0100

    lol

--- www/actions/form_register.php
+++ www/actions/form_register.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../../src/init.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
-    $iban = $_POST['iban'];
+    // $iban = $_POST['iban'];
     $birthdate = $_POST['birthdate'];
     $email = $_POST['mail'];
     $password = $_POST['password'];
@@ -24,8 +24,6 @@ if ($user->rowCount() > 0) {
 } else {
     $password = password_hash($password, PASSWORD_DEFAULT);
 
-    // $sql = "INSERT INTO users (firstname, lastname, iban, birthdate, mail, password) VALUES (?, ?, ?, ?, ?, ?)";
-    // $idInserted = $dbManager->insert($sql, [$firstname, $lastname, $iban, $birthdate, $email, $password]);
     createAccount($firstname, $lastname, $email, $password, $birthdate);
 
     echo "Inscription réussie!!!";
